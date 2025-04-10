c6b23ad99132ec85ecac05710ed46381bc7fbfaa
paili2 <zxcv7502@naver.com>
2025-04-10 15:06:07 +0900

    　

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/ticketLink-clone", // {repository-name}을 실제 저장소 이름으로 대체
+});
